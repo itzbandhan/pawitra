ca0f43b72c129e51b3604eef958f93f19d9eb206
ItzBandhan <105062360+itzbandhan@users.noreply.github.com>
2024-09-11 08:26:00 +0545

    Update Partners.jsx

--- src/Components/Partners.jsx
+++ src/Components/Partners.jsx
@@ -5,10 +5,14 @@ export default function Partners() {
         <h2 className="text-center text-lg font-semibold leading-8 text-gray-900">
           Partnered with most of the creative teams
         </h2>
+       
         <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
           <img
             alt="Reform"
+             {/*
             src="https://images.squarespace-cdn.com/content/v1/64129f477e53173c1e7eccc2/37fa17b2-2caa-4408-be50-b19609187ea4/orange-leaf-logo-1000x1000.png?format=1500w"
+          */}
+            src="https://cdn.jsdelivr.net/gh/itzbandhan/projectvids/download.png"
             width={158}
             height={48}
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
